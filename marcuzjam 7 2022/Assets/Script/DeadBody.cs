6026f4b0a5329cf508f46df2c899a1c04be5117c
MosleyDev <90133380+MosleyDev@users.noreply.github.com>
2022-07-26 21:13:19 +0300

    spring joint

--- marcuzjam 7 2022/Assets/Script/DeadBody.cs
+++ marcuzjam 7 2022/Assets/Script/DeadBody.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class DeadBody : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Lava"))
         {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
             GetComponent<SpringJoint2D>().enabled = true;
+            GetComponent<SpringJoint2D>().connectedAnchor = new Vector2(this.transform.position.x, this.transform.position.y + 0.5f);
 
         }
     }
