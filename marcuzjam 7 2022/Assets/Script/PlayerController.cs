66e377498978a3899eb7f2ac06bbe44d4d43c4fc
MosleyDev <90133380+MosleyDev@users.noreply.github.com>
2022-07-26 19:16:39 +0300

    body grab and drop

--- marcuzjam 7 2022/Assets/Script/PlayerController.cs
+++ marcuzjam 7 2022/Assets/Script/PlayerController.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using PixelAnimator;
 
-public class PlayerController : MonoBehaviour{
+public class PlayerController : MonoBehaviour
+{
 
     [SerializeField] private SpriteAnimation run;
     [SerializeField] private SpriteAnimation idle;
     [SerializeField] private SpriteAnimation runGrab_Up;
+    [SerializeField] private SpriteAnimation runGrab_RightAndLeft;
+    [SerializeField] private SpriteAnimation idleGrab_Up;
+    [SerializeField] private SpriteAnimation idleGrab_RightAndLeft;
     [SerializeField] private AnimationManager anim;
-    
+
     [Space(20)]
     [SerializeField] Rigidbody2D body;
 
@@ -18,34 +22,35 @@ public class PlayerController : MonoBehaviour{
     private float inputRaw;
     private bool isRun;
 
-    private void Start() {
-        
-
-
-    }
-
-    private void Update() {
+    public bool isGrabbingBody;
+    public bool isGrabbingUp = true;
+    private void Update()
+    {
         inputRaw = Input.GetAxisRaw("Horizontal");
 
-        isRun = Mathf.Abs(inputRaw) > 0 ? true : false; 
+        isRun = Mathf.Abs(inputRaw) > 0 ? true : false;
 
-        if(Mathf.Abs(inputRaw) > 0){
+        if (Mathf.Abs(inputRaw) > 0)
+        {
 
-            anim.ChangeState(run);
+            anim.ChangeState(isGrabbingBody ? (isGrabbingUp ? runGrab_Up : runGrab_RightAndLeft) : run);
 
-        }else{
-            anim.ChangeState(idle);
+        }
+        else
+        {
+            anim.ChangeState(isGrabbingBody ? (isGrabbingUp ? idleGrab_Up : idleGrab_RightAndLeft) : idle);
         }
 
-        if(inputRaw < 0)
+        if (inputRaw < 0)
             this.transform.rotation = Quaternion.Euler(0, 180, 0);
-        else if(inputRaw > 0)
+        else if (inputRaw > 0)
             this.transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 
 
-    private void FixedUpdate() {
-        
+    private void FixedUpdate()
+    {
+
         body.velocity = new Vector2(inputRaw * speed, body.velocity.y);
 
 
