4919d0c495f7848504dc0f4452698e22d5143b3e
MosleyDev <90133380+MosleyDev@users.noreply.github.com>
2022-07-26 16:46:21 +0300

    spinning knives

---
+++ marcuzjam 7 2022/Assets/Script/SpinningKnive.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningKnive : MonoBehaviour
+{
+    [SerializeField] LayerMask _layer;
+    [SerializeField] private float _speed, _rayDistance;
+    Vector2 _currentDirection;
+    Rigidbody2D _rigidbody2D;
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _currentDirection = transform.right;
+        _rigidbody2D.velocity = _currentDirection * _speed;
+    }
+    void SwitchDirection()
+    {
+        _currentDirection = -_currentDirection;
+        _rigidbody2D.velocity = _currentDirection * _speed;
+    }
+
+    private void Update()
+    {
+        if (Physics2D.Raycast(transform.position, _currentDirection, _rayDistance, _layer))
+        {
+            SwitchDirection();
+        }
+    }
+}
