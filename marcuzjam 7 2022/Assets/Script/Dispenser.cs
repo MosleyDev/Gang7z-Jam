50d5ce79846793b3b844ab259d130fe467a92eb3
MosleyDev <90133380+MosleyDev@users.noreply.github.com>
2022-07-26 17:21:56 +0300

    dispanser ve spinning knives

--- marcuzjam 7 2022/Assets/Script/Dispenser.cs
+++ marcuzjam 7 2022/Assets/Script/Dispenser.cs
@@ -6,8 +6,21 @@ public class Dispenser : MonoBehaviour
 {
     [SerializeField] private DispenserDirection _dispenserDirection;
     [SerializeField] private GameObject _arrowPrefab;
-    [SerializeField] private float _arrowSpeed;
+    [SerializeField] private float _arrowSpeed, _spawnDuration;
+    [SerializeField] private Transform _upSP, _rightSP, _downSP, _leftSP;
 
+    private void Start()
+    {
+        StartCoroutine(ArrowLoop());
+    }
+    IEnumerator ArrowLoop()
+    {
+        while (true)
+        {
+            SpawnArrow();
+            yield return new WaitForSeconds(_spawnDuration);
+        }
+    }
     private void SpawnArrow()
     {
         Vector2 spawnDirection = _dispenserDirection switch
@@ -19,7 +32,16 @@ public class Dispenser : MonoBehaviour
             _ => transform.up
         };
 
-        GameObject newArrow = Instantiate(_arrowPrefab, transform.position, Quaternion.AngleAxis(0, spawnDirection));
+        Vector2 spawnPosition = _dispenserDirection switch
+        {
+            DispenserDirection.up => _upSP.position,
+            DispenserDirection.right => _rightSP.position,
+            DispenserDirection.down => _downSP.position,
+            DispenserDirection.left => _leftSP.position,
+            _ => _upSP.position
+        };
+
+        GameObject newArrow = Instantiate(_arrowPrefab, spawnPosition, Quaternion.AngleAxis(0, spawnDirection));
 
         newArrow.GetComponent<Rigidbody2D>().velocity = spawnDirection * _arrowSpeed;
     }
